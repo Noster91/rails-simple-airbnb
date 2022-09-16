0eea9d809c560f08cdbf3eb3ab22744516b94465
julian chadwick <noster91@gmail.com>
2022-09-15 21:28:54 -0300

    Controller + seed de Flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Flat.create(name: Faker::Movie.title, address: Faker::Address.street_address, description: "Some random description", price_per_night: 10, number_of_guests: 2)
+Flat.create(name: Faker::Movie.title, address: Faker::Address.street_address, description: "Some random description 2", price_per_night: 30, number_of_guests: 4)
+Flat.create(name: Faker::Movie.title, address: Faker::Address.street_address, description: "Some random description 3", price_per_night: 20, number_of_guests: 1)
+Flat.create(name: Faker::Movie.title, address: Faker::Address.street_address, description: "Some random description 4", price_per_night: 50, number_of_guests: 5)
